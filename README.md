03132e1de28802bdbe01303513c6ae15bed3da9d
yunha <wjh0970@gmail.com>
2022-10-22 23:56:20 +0900

    4rd

--- README.md
+++ README.md
@@ -5,4 +5,10 @@
 3. 곱하기 - `multiply.js`
 4. 나누기(몫 구하기) - `quotient.js`
 5. 나누기(나머지 구하기) - `persent.js`
-6. 
+6. 나누기(몫 * 1000 을 정수로 출력) - `divide.js`
+7. 숫자 비교 - `compare.js`
+8. 분수 덧셈 - `fraction.js`
+9. 배열 2배 - `array2x.js`
+10. 배열 중앙값 - `median.js`
+11. 배열 최빈값 - `mode.js` - 참고[https://velog.io/@ge-um/%EC%B5%9C%EB%B9%88%EA%B0%92-%EA%B5%AC%ED%95%98%EA%B8%B0]최빈값구하기
+12. 
--- mode.js
+++ mode.js
@@ -4,13 +4,15 @@
  * @returns
  */
 function mode(array) {
-  var answer = 0;
   let arr = new Map();
-  for(let i = 0; i<array.length; i++){
-    arr.set(array[i], (arr.get(array[i])||0) + 1);
+  for (let i = 0; i < array.length; i++) {
+    arr.set(array[i], (arr.get(array[i]) || 0) + 1);
   }
+  let max = Math.max(...[...arr].map((v) => v[1]));
+  let count = [...arr].filter((v) => v[1] === max).length;
 
-  return arr;
+  var answer = count > 1 ? -1 : [...arr].find((v) => v[1] === max)[0];
+  return answer;
 }
 console.log(mode([1, 2, 3, 3, 3, 4]));
 console.log(mode([1, 1, 2, 2]));
