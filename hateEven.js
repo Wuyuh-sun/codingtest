bc3e71456203192222c1e5db1e6a93872f4cd136
yunha <wjh0970@gmail.com>
2022-12-24 22:48:47 +0900

    hateEven

---
+++ hateEven.js
@@ -0,0 +1,18 @@
+/**
+ * 정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+ * @param {number} n 정수
+ * @returns
+ */
+function hateEven(n) {
+  var answer = [];
+  for(let i=0; i<=n; i++){
+    if(i%2 === 0){
+      false;
+    } else{
+      answer.push(i);
+    }
+  }
+  return answer;
+}
+console.log(hateEven(10));
+console.log(hateEven(15));
